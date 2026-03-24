3475651990c6d7a2924d41d0222f523c3d0911eb
flavienrak <flavien.andrisoarak@gmail.com>
2026-03-24 12:16:24 +0300

    feat(init): add initial configs

---
+++ routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Laravel\Fortify\Features;
+use Illuminate\Support\Facades\Session;
+
+Route::inertia('/', 'welcome', [
+    'canRegister' => Features::enabled(Features::registration()),
+])->name('home');
+
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::inertia('dashboard', 'dashboard')->name('dashboard');
+});
+
+Route::get('language/{locale}', function ($locale) {
+    if (in_array($locale, ['en', 'fr'])) {
+        Session::put('locale', $locale);
+    }
+    return redirect()->back();
+});
+
+require __DIR__.'/settings.php';
